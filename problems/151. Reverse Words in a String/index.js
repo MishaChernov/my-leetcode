39e771cdccd3a150ac6de469627105e1e6445f60
Mykhailo Chernov <m.chernov@astoundcommerce.com>
2023-02-07 12:46:12 +0100

    151. Reverse Words in a String

---
+++ problems/151. Reverse Words in a String/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+    return s.split(' ').filter(Boolean).reverse().join(' ');
+};
+
+console.log(reverseWords('  Hello World  '));
