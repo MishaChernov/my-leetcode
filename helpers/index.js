c549948107d8ef3fe39ca1f259e698ff05686ac4
Mykhailo Chernov <m.chernov@astoundcommerce.com>
2023-03-13 19:23:12 +0100

    Add arrayToTree helper function

--- helpers/index.js
+++ helpers/index.js
@@ -5,6 +5,12 @@ function ListNode(val, next) {
     this.next = next === undefined ? null : next;
 }
 
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+  }
+
 function createLinkedListFromArray(array) {
     const list = new ListNode(+array[0]);
     let nextItem = list;
@@ -18,7 +24,43 @@ function createLinkedListFromArray(array) {
     return list;
 }
 
+function arrayToTree(arr) {
+    if (!arr.length) {
+      return null;
+    }
+  
+    const root = new TreeNode(arr[0]);
+    const queue = [root];
+    let i = 1;
+  
+    while (queue.length && i < arr.length) {
+      const node = queue.shift();
+  
+      if (arr[i] !== null) {
+        node.left = new TreeNode(arr[i]);
+        queue.push(node.left);
+      }
+  
+      i++;
+  
+      if (i >= arr.length) {
+        break;
+      }
+  
+      if (arr[i] !== null) {
+        node.right = new TreeNode(arr[i]);
+        queue.push(node.right);
+      }
+  
+      i++;
+    }
+  
+    return root;
+  }
+
 module.exports = {
     ListNode,
+    TreeNode,
     createLinkedListFromArray,
+    arrayToTree
 };
