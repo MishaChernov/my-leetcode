3ad27a1ac819411950babeb4681f11d297bf688a
Mykhailo Chernov <m.chernov@astoundcommerce.com>
2023-03-27 21:55:12 +0200

    106. Construct Binary Tree from Inorder and Postorder Traversal

---
+++ problems/106. Construct Binary Tree from Inorder and Postorder Traversal/index.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} inorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+var buildTree = function(inorder, postorder) {
+  if (!inorder.length || !postorder.length) return null;
+
+  const root = new TreeNode(postorder[postorder.length - 1]);
+  const mid = inorder.indexOf(postorder[postorder.length - 1]);
+
+  root.left = buildTree(inorder.slice(0, mid), postorder.slice(0, mid));
+  root.right = buildTree(inorder.slice(mid + 1), postorder.slice(mid, -1));
+
+  return root;
+};
+
+console.log(buildTree([9,3,15,20,7], [9,15,7,20,3]))
